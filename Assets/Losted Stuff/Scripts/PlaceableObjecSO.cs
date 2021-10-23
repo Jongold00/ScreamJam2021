2bfbee3e58d4250630dc1bddd0c21791c4611231
Alexandre Alves <pynjunior@gmail.com>
2021-10-23 01:03:02 +0100

    Added Hotkey to change type of fences

---
+++ Assets/Losted Stuff/Scripts/PlaceableObjecSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class PlaceableObjecSO : ScriptableObject
+{
+    public GameObject prefabs;
+    public GameObject prefabsPreview;
+}
