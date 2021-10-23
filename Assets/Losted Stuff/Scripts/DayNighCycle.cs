334da01ff2f104d9fd11b5c2d74dcafaedef2a60
Alexandre Alves <pynjunior@gmail.com>
2021-10-23 18:59:03 +0100

    DayNightController

--- Assets/Losted Stuff/Scripts/DayNighCycle.cs
+++ Assets/Losted Stuff/Scripts/DayNighCycle.cs
@@ -1,16 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DayNighCycle : MonoBehaviour
 {
     public Material dayMaterial;
     public Material nightMaterial;
-    float duration = 10000.0f;
+
+    [SerializeField] [Range(0, 5)] private float lerpTime;
+    bool change;
+
+    private void Start()
+    {
+
+    }
 
     private void Update()
     {
-        float lerp = Mathf.PingPong(Time.time, duration) / duration;
-        RenderSettings.skybox.Lerp(nightMaterial, dayMaterial, lerp);
+        if (Input.GetMouseButtonDown(0))
+            change = true;
+
+        if (change)
+        {
+            RenderSettings.skybox = dayMaterial;
+            RenderSettings.skybox.Lerp(dayMaterial, nightMaterial, lerpTime * Time.deltaTime);
+        }
+        else
+        {
+            RenderSettings.skybox = nightMaterial;
+            RenderSettings.skybox.Lerp(nightMaterial, dayMaterial, lerpTime * Time.deltaTime);
+        }
     }
 }
